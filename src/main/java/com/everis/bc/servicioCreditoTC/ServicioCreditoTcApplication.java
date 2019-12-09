03151ef89bc8237dd0a586e429f4ba54c0e1d5c8
fjosescarmona <fjosescarmona@gmail.com>
2019-12-09 17:55:37 -0500

    primera carga servicio credito TC

---
+++ src/main/java/com/everis/bc/servicioCreditoTC/ServicioCreditoTcApplication.java
@@ -0,0 +1,13 @@
+package com.everis.bc.servicioCreditoTC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioCreditoTcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioCreditoTcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everis/bc/servicioCreditoTC/ServicioCreditoTcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.bc.servicioCreditoTC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioCreditoTcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
